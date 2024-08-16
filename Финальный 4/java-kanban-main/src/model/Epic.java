10a672f5a11d59c7541b1c689888b71cf633fbbd
Pavel_RK97 <79162012+pavelrk97@users.noreply.github.com>
2024-08-16 17:07:59 +0300

    Add files via upload
    Финальное задание 4 спринта, -main сстарое

---
+++ Финальный 4/java-kanban-main/src/model/Epic.java
@@ -0,0 +1,39 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Epic extends Task {
+    private final List<Integer> subtasks = new ArrayList<>();
+
+    public Epic(String name, String description) {
+        super(name, description);
+    }
+
+    public List<Integer> getSubtasks() {
+        return subtasks;
+    }
+
+    public void addSubtask(Integer subtaskId) {
+        subtasks.add(subtaskId);
+    }
+
+    public void deleteSubtask(Integer subtaskId) {
+        subtasks.remove(subtaskId);
+    }
+
+    public void removeSubtasks() {
+        subtasks.clear();
+    }
+
+    @Override
+    public String toString() {
+        return "Epic{" +
+                "subtasks=" + getSubtasks() +
+                ", id=" + getId() +
+                ", name='" + getName() + '\'' +
+                ", description='" + getDescription() + '\'' +
+                ", taskStatus=" + getStatus() +
+                '}';
+    }
+}
---
+++ Финальный 4/java-kanban/src/Subtask.java
@@ -0,0 +1,24 @@
+public class Subtask extends Task {
+    private final int epicId;
+
+    public Subtask(String name, String description, Status status, int epicId) {
+        super(name, description, status);
+        this.epicId = epicId;
+    }
+
+    public int getEpicId() {
+        return epicId;
+    }
+
+    @Override
+    public String toString() {
+        return "Subtask{" +
+                "epicId=" + getEpicId() +
+                ", id=" + getId() +
+                ", name='" + getName() + '\'' +
+                ", description='" + getDescription() + '\'' +
+                ", taskStatus=" + getStatus() +
+                '}';
+    }
+}
+
